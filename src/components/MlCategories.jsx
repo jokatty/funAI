b6dfa2557feefe03417fb9887c39c07db70b2932
jokatty <jyotikattani@gmail.com>
2022-04-25 14:55:34 +0200

    add ExploreCategoryModal

--- src/components/MlCategories.jsx
+++ src/components/MlCategories.jsx
@@ -3,35 +3,12 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-
-//  list the static images for the page
-const itemData = [
-  {
-    img:
-      'https://www.freelancinggig.com/blog/wp-content/uploads/2017/08/Programming-Languages-For-Face-Recognition.jpg',
-    title: 'Face Detection',
-  },
-  {
-    img:
-      'https://upload.wikimedia.org/wikipedia/commons/3/38/Detected-with-YOLO--Schreibtisch-mit-Objekten.jpg',
-    title: 'Object Detection',
-  },
-  {
-    img:
-      'https://www.researchgate.net/profile/Md-Islam-1141/publication/331373648/figure/fig1/AS:730814132264965@1551251075247/Applications-of-text-detection-and-recognition-a-Robotic-navigation-b-Automatic_Q320.jpg',
-    title: 'Text Detection',
-  },
-  {
-    img:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS55jwRrZOTObvtHSXQRQMbi_QACFKYMtHogA&usqp=CAU',
-    title: 'Logo Detection',
-  },
-];
+import ExploreCagoryModal from './ExploreCategoryModal';
+import itemData from '../categoryImages';
 
 export default function MlCategories() {
   return (
-    // sx={{ width: 500, height: 450 }}
-    <ImageList sx={{ width: 500, height: 450 }} cols={4} rowHeight="200">
+    <ImageList>
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
           <img
@@ -44,6 +21,7 @@ export default function MlCategories() {
             title={item.title}
             position="below"
           />
+          <ExploreCagoryModal item={item.title} />
         </ImageListItem>
       ))}
     </ImageList>
