042eca1c31ec9ceabb2fcc4f56db9d876ecab884
jokatty <jyotikattani@gmail.com>
2022-04-25 14:53:01 +0200

    add all the category images

---
+++ src/categoryImages.js
@@ -0,0 +1,24 @@
+//  list the static images for the page
+const itemData = [
+  {
+    img:
+      'https://www.freelancinggig.com/blog/wp-content/uploads/2017/08/Programming-Languages-For-Face-Recognition.jpg',
+    title: 'Face Detection',
+  },
+  {
+    img:
+      'https://upload.wikimedia.org/wikipedia/commons/3/38/Detected-with-YOLO--Schreibtisch-mit-Objekten.jpg',
+    title: 'Object Detection',
+  },
+  {
+    img:
+      'https://www.researchgate.net/profile/Md-Islam-1141/publication/331373648/figure/fig1/AS:730814132264965@1551251075247/Applications-of-text-detection-and-recognition-a-Robotic-navigation-b-Automatic_Q320.jpg',
+    title: 'Text Detection',
+  },
+  {
+    img:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS55jwRrZOTObvtHSXQRQMbi_QACFKYMtHogA&usqp=CAU',
+    title: 'Logo Detection',
+  },
+];
+export default itemData;
